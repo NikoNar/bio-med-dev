9f7f65820764020a9929148e522bb49d495d4a0f
Mikayel <abachyanm@gmail.com>
2021-08-07 18:26:23 +0400

    loacale bug fix

--- components/AccountForms/RegisterForm/RegisterForm.jsx
+++ components/AccountForms/RegisterForm/RegisterForm.jsx
@@ -16,13 +16,14 @@ const RegisterForm = ({security, currentUser}) => {
     const nameRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     const {t} = useTranslation()
+
     const [isOpen, setIsOpen] = useState(false)
-    const [resError, setResError] = useState('')
+    const [resError, setResError] = useState()
     const registerSchema = Yup.object().shape({
         first_name: Yup.string().matches(/^([^1-9]*)$/).required(t('errors:name_error')),
         registerGender: Yup.string().nullable(true).required(t('errors:gender_error')),
         registerDate: Yup.string().required(t('errors:birthday_error')),
-        email: Yup.string().matches(nameRegex, t('errors:name_language_error')).email(t('errors:email_format_error')).required(t('errors:enter_email')),
+        email: Yup.string().matches(nameRegex, t('errors:language_error')).email(t('errors:email_format_error')).required(t('errors:enter_email')),
         registerPhone: Yup.string().required(t('errors:phone_error')),
         password: Yup.string().min(6, t('errors:password_min_error')).max(16, t('errors:password_max_error')).required(),
         registerConfirmPassword: Yup.string().oneOf([Yup.ref('password'), null], t('errors:confirm_password_error'))
@@ -45,16 +46,12 @@ const RegisterForm = ({security, currentUser}) => {
     );
 
     useEffect(() => {
-        //setIsOpen(!!errors)
         if (isOpen) {
             document.body.style.overflow = 'hidden';
         } else {
             document.body.style.overflow = 'unset';
         }
     }, [isOpen]);
-
-    console.log(errors)
-
 
     const registerHandleSubmit = async (registerData) => {
 
